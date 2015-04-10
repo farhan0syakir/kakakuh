4e17491eb38e69c35f677e29d54a5a652db5f88b
anaszakariyah <anas.zakariyah@gmail.com>
2015-04-10 16:55:35 +0700

    UBah LOGIN ACTIVITY dan sudah dibuat xml BUAT AKUN
    Login activity disitu ada todo. masukin code login disitu aja.
    buatakun fragment juga ada. aku tandain todo biar gampang. Semangat!

--- app/src/main/java/com/kakakuh/c4ppl/kakakuh/BuatAkunFragment.java
+++ app/src/main/java/com/kakakuh/c4ppl/kakakuh/BuatAkunFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 /**
  * Created by Anas on 4/2/2015.
@@ -16,6 +19,36 @@ public class BuatAkunFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_buat_akun, container, false);
+
+        RadioGroup radioAkun = (RadioGroup) rootView.findViewById(R.id.radio_group);
+
+        radioAkun.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radio_kakak:
+                        // 'Kakak Asuh' checked
+                        //TODO
+                        System.out.println("Kakak"); //TEST
+                        break;
+                    case R.id.radio_adik:
+                        // 'Adik Asuh' checked
+                        //TODO
+                        System.out.println("Adik"); //TEST
+                        break;
+                }
+            }
+        });
+
+        Button btnBuat = (Button) rootView.findViewById(R.id.btn_buat);
+        btnBuat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Buat Akun ngapain
+                //TODO
+                System.out.println("Haalloo"); //TEST
+            }
+        });
 
         return rootView;
     }
--- app/src/main/java/com/kakakuh/c4ppl/kakakuh/LoginActivity.java
+++ app/src/main/java/com/kakakuh/c4ppl/kakakuh/LoginActivity.java
@@ -3,6 +3,7 @@ package com.kakakuh.c4ppl.kakakuh;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 import android.view.View;
@@ -19,29 +20,29 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.activity_login);
         usernameField = (EditText)findViewById(R.id.username);
         passwordField = (EditText)findViewById(R.id.password);
+
+        Button btnLogin = (Button) findViewById(R.id.btn_buat);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Buat Akun ngapain
+                //TODO
+                System.out.println("Login"); //TEST
+
+                //ini entah bekas. tapi emg eksekusinya disini
+                /*
+                String username = usernameField.getText().toString();
+                String password = passwordField.getText().toString();
+                new SigninActivity().execute(username, password); //ini seharusnya kelas LoginController..
+
+                if(username.equals("admin") && password.equals("admin")){
+                    Intent nextScreen = new Intent(getApplicationContext(), Blank.class);
+
+                    startActivity(nextScreen);
+                }
+                */
+            }
+        });
     }
-
-    public void login(View view){
-        String username = usernameField.getText().toString();
-        String password = passwordField.getText().toString();
-        new SigninActivity().execute(username, password);
-
-        if(username.equals("admin") && password.equals("admin")){
-            Intent nextScreen = new Intent(getApplicationContext(), Blank.class);
-
-            startActivity(nextScreen);
-        }
-    }
-
-
-    /*
-    public void showSoftKeyboard(View view) {
-        if (view.requestFocus()) {
-            InputMethodManager imm = (InputMethodManager)
-                    getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
-        }
-    }
-    */
 }
 
