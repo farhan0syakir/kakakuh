4c8f2f8e519da53a1ecb493b40cde0a06bde4cc0
anaszakariyah <anas.zakariyah@gmail.com>
2015-04-12 23:32:16 +0700

    FIXED

---
+++ app/src/main/java/com/kakakuh/c4ppl/kakakuh/BuatAkunFragment.java
@@ -0,0 +1,166 @@
+package com.kakakuh.c4ppl.kakakuh;
+
+import android.app.Fragment;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class BuatAkunFragment extends Fragment{
+    public BuatAkunFragment(){}
+
+    private EditText usernameField, passwordField;
+    String user="",pass="",id;
+    String role = "0";
+    InputStream is=null;
+    String result=null;
+    String line=null;
+    int code;
+    private String url1 = "http://ppl-c04.cs.ui.ac.id/index.php/mengelolaAkunController/create";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_buat_akun, container, false);
+
+        final RadioGroup radioAkun = (RadioGroup) rootView.findViewById(R.id.radio_group);
+
+        radioAkun.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radio_kakak:
+                        // 'Kakak Asuh' checked
+                        //TODO
+                        role = "1";
+                        break;
+                    case R.id.radio_adik:
+                        // 'Adik Asuh' checked
+                        //TODO
+                        role = "2";
+                        break;
+                }
+            }
+        });
+
+        usernameField = (EditText)rootView.findViewById(R.id.username);
+        passwordField = (EditText)rootView.findViewById(R.id.password);
+
+
+        Button btnBuat = (Button) rootView.findViewById(R.id.btn_buat);
+        btnBuat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Buat Akun ngapain
+//                TODO
+                user = usernameField.getText().toString();
+                pass = passwordField.getText().toString();
+                //debug
+                System.out.println(user);
+                System.out.println(pass);
+                if(user.equals("")||pass.equals("")||role.equals("0")) {
+                    Toast.makeText(getActivity(), "Ada yang belum terisi",
+                            Toast.LENGTH_LONG).show();
+                }
+                else{
+                    new insertTask().execute("");
+                    Toast.makeText(getActivity(), "Akun Berhasil Dibuat",
+                            Toast.LENGTH_LONG).show();
+                    usernameField.setText("");
+                    passwordField.setText("");
+                    radioAkun.clearCheck();
+                }
+            }
+        });
+
+        return rootView;
+    }
+
+
+    public String insert() {
+
+            ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+
+            nameValuePairs.add(new BasicNameValuePair("username", user));
+            nameValuePairs.add(new BasicNameValuePair("password", pass));
+            nameValuePairs.add(new BasicNameValuePair("role", role));
+            //debug
+            System.out.println(nameValuePairs);
+
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost httppost = new HttpPost(url1);
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                HttpResponse response = httpclient.execute(httppost);
+                HttpEntity entity = response.getEntity();
+                is = entity.getContent();
+                //debug
+                System.out.println(is);
+                Log.e("pass 1", "connection success ");
+            } catch (Exception e) {
+                Log.e("Fail 1", e.toString());
+                Toast.makeText(getActivity(), "Invalid IP Address",
+                        Toast.LENGTH_LONG).show();
+
+            }
+
+            try {
+                BufferedReader reader = new BufferedReader
+                        (new InputStreamReader(is, "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                is.close();
+                result = sb.toString();
+                //debug
+
+                Log.e("pass 2", "connection success ");
+                System.out.println(result);
+            } catch (Exception e) {
+                Log.e("Fail 2", e.toString());
+
+            }
+
+          return result;
+    }
+
+    class insertTask extends AsyncTask<String, String, String> {
+        protected String doInBackground(String... params) {
+            String hasil = insert();
+            return hasil ;
+        }
+
+
+
+        protected void onPostExecute(String result) {
+            if(result.equals("OK"))
+                System.out.print("ea");
+            //after background is done, use this to show or hide dialogs
+        }
+    }
+}
+
+
+
---
+++ app/src/main/java/com/kakakuh/c4ppl/kakakuh/model/AkunListItem.java
@@ -0,0 +1,52 @@
+package com.kakakuh.c4ppl.kakakuh.model;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Anas on 4/11/2015.
+ */
+public class AkunListItem {
+    private String username;
+    private String name;
+    private String role;
+    private Bitmap photo;
+
+    public AkunListItem(String keyUsername, String name, String role, Bitmap photo) {
+        this.name = name;
+        this.role = role;
+        this.photo = photo;
+        this.username = keyUsername;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Bitmap getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Bitmap photo) {
+        this.photo = photo;
+    }
+}
