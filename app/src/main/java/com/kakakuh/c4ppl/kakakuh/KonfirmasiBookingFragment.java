b2409e6a2cbb80f7a0dfacd807fa5d90575fa42d
Yoga Widyakrisna <yoga.widyakrisna@gmail.com>
2015-06-13 02:59:59 +0700

    Konfirmasi Booking sudah bisa

--- app/src/main/java/com/kakakuh/c4ppl/kakakuh/KonfirmasiBookingFragment.java
+++ app/src/main/java/com/kakakuh/c4ppl/kakakuh/KonfirmasiBookingFragment.java
@@ -1,6 +1,8 @@
 package com.kakakuh.c4ppl.kakakuh;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,9 +10,20 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.kakakuh.c4ppl.kakakuh.controller.BookingListAdapter;
+import com.kakakuh.c4ppl.kakakuh.controller.ImageConverter;
+import com.kakakuh.c4ppl.kakakuh.controller.KakakuhBaseJSONParserAsyncTask;
+import com.kakakuh.c4ppl.kakakuh.controller.Kalender;
+import com.kakakuh.c4ppl.kakakuh.controller.Preferensi;
 import com.kakakuh.c4ppl.kakakuh.model.BookingListItem;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -21,6 +34,14 @@ public class KonfirmasiBookingFragment extends Fragment{
     private ArrayList<BookingListItem> konfirmasiListItems;
     private BookingListAdapter adapter;
     private ListView mListBooking;
+    private JSONArray jsonArray = null;
+    private String user;
+    private Bitmap decodedByte;
+    private String dateTxt;
+
+    private Preferensi preferensi;
+
+
     public KonfirmasiBookingFragment(){}
 
     @Override
@@ -33,16 +54,89 @@ public class KonfirmasiBookingFragment extends Fragment{
         mListBooking.setFocusable(false);
         mListBooking.setFocusableInTouchMode(false);
         mListBooking.setClickable(false);
-        Date date = new Date();
-        konfirmasiListItems = new ArrayList<>();
-        konfirmasiListItems.add(new BookingListItem("kakak","adik",1,true,date,date,date));
-        konfirmasiListItems.add(new BookingListItem("kakak","adik",1,true,date,date,date));
-        konfirmasiListItems.add(new BookingListItem("kakak","adik",1,true,date,date,date));
 
-        adapter = new BookingListAdapter(getActivity(), konfirmasiListItems);
-        System.out.println("ini adapter "+adapter);
-        System.out.println("ini adapter "+konfirmasiListItems);
-        mListBooking.setAdapter(adapter);
+        preferensi = new Preferensi(getActivity());
+        user = preferensi.getUsername();
+        new JSONParser(getActivity(),"http://ppl-c04.cs.ui.ac.id/index.php/bookingController?userkakak="+user).execute();
+
+//        Date date = new Date();
+//        konfirmasiListItems.add(new BookingListItem("kakak","adik",1,true,date,date,date));
+//        konfirmasiListItems.add(new BookingListItem("kakak","adik",1,true,date,date,date));
+//        konfirmasiListItems.add(new BookingListItem("kakak","adik",1,true,date,date,date));
+//
+//        adapter = new BookingListAdapter(getActivity(), konfirmasiListItems);
+//        System.out.println("ini adapter "+adapter);
+//        System.out.println("ini adapter "+konfirmasiListItems);
+//        mListBooking.setAdapter(adapter);
         return rootView;
     }
+
+    class JSONParser extends KakakuhBaseJSONParserAsyncTask {
+
+        public JSONParser(Context context, String url) {
+            super(context, url);
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            pDialog.dismiss();
+            try {
+
+                konfirmasiListItems = new ArrayList<>();
+                // Getting JSON Array from URL
+                jsonArray = json.getJSONArray("data");
+                for(int i = 0; i < jsonArray.length(); i++){
+                    JSONObject c = jsonArray.getJSONObject(i);
+                    // Storing  JSON item in a Variable
+                    String useradik = c.getString("useradik");
+                    String userkakak = c.getString("userkakak");
+                    String idjadwal = c.getString("id_jadwal");
+                    String bookstatus = c.getString("book_status");
+                    decodedByte = ImageConverter.convertStringToBitmap(c.getString("img"));
+                    String txttimestamp = c.getString("timestamp");
+                    String txtstart = c.getString("start_date");
+                    String txtend = c.getString("end_date");
+                    String myDateSqlFormat = "yyyy-MM-dd HH:mm:ss";
+
+                    Date parsedStartDate = new SimpleDateFormat(myDateSqlFormat).parse(txtstart);
+                    Date parsedEndDate = new SimpleDateFormat(myDateSqlFormat).parse(txtend);
+
+                    Calendar startTime = Calendar.getInstance();
+                    startTime.setTime(parsedStartDate);
+                    Calendar endTime = Calendar.getInstance();
+                    endTime.setTime(parsedEndDate);
+                    String[] timeStart = Kalender.getTimeBooking(startTime);
+                    String hariStart = timeStart[0];
+                    String waktuStart = timeStart[1];
+                    String[] timeEnd = Kalender.getTimeBooking(endTime);
+                    String hariEnd = timeEnd[0];
+                    String waktuEnd = timeEnd[1];
+
+                    if(startTime.get(Calendar.YEAR)==endTime.get(Calendar.YEAR)&&startTime.get(Calendar.DAY_OF_YEAR)==endTime.get(Calendar.DAY_OF_YEAR)){
+                        dateTxt = "Ingin melakukan booking pada " + hariStart + " di jam " + waktuStart + "-"+ waktuEnd;
+                    }
+                    else{
+                        dateTxt = "Ingin melakukan booking pada " + hariStart + " di jam " + waktuStart + " sampai" + hariEnd + " di jam " + waktuEnd ;
+                    }
+//                    String datetxt = "Ingin melakukan booking pada hari" + txtstart + " sampai" + txtend;
+                    BookingListItem booking = new BookingListItem(useradik,userkakak,Integer.parseInt(idjadwal), txttimestamp, dateTxt,decodedByte);
+                    if(bookstatus.equals("2")){
+                        konfirmasiListItems.add(booking);
+                    }
+                    else{
+
+                    }
+                }
+                adapter = new BookingListAdapter(getActivity(), konfirmasiListItems);
+                mListBooking.setAdapter(adapter);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/com/kakakuh/c4ppl/kakakuh/controller/BookingListAdapter.java
+++ app/src/main/java/com/kakakuh/c4ppl/kakakuh/controller/BookingListAdapter.java
@@ -2,22 +2,43 @@ package com.kakakuh.c4ppl.kakakuh.controller;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.kakakuh.c4ppl.kakakuh.R;
 import com.kakakuh.c4ppl.kakakuh.model.BookingListItem;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
  * Created by Anas on 4/11/2015.
  */
 public class BookingListAdapter extends KakakuhBaseAdapter<BookingListItem> {
+    private String username = "";
+    private String url = "http://ppl-c04.cs.ui.ac.id/index.php/bookingController/jawab?";//+jawaban=1&userkakak=kakak&useradik=adik&id_jadwal=45
+    InputStream is = null;
+    String result = null;
+    String line = null;
+    int id=0;
+    String useradik = null;
+    String userkakak = null;
+
 
     public BookingListAdapter(Context context, ArrayList<BookingListItem> bookingListItems) {
         this.context = context;
@@ -25,7 +46,7 @@ public class BookingListAdapter extends KakakuhBaseAdapter<BookingListItem> {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
             LayoutInflater mInflater = (LayoutInflater)
                     context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
@@ -36,13 +57,160 @@ public class BookingListAdapter extends KakakuhBaseAdapter<BookingListItem> {
         TextView txtNamaAkun = (TextView) convertView.findViewById(R.id.nama_akun);
         TextView txtTugas= (TextView) convertView.findViewById(R.id.tugas);
         TextView txtTanyaTerima= (TextView) convertView.findViewById(R.id.tanya_terima);
-        Button tidak = (Button) convertView.findViewById(R.id.btn_tidak);
-        Button ya = (Button) convertView.findViewById(R.id.btn_ya);
+        Button btnTidak = (Button) convertView.findViewById(R.id.btn_tidak);
+        Button btnYa = (Button) convertView.findViewById(R.id.btn_ya);
 
-        txtNamaAkun.setText("Akun");
-        txtTugas.setText("tugas");
-        txtTanyaTerima.setText("terima?");
+
+        final BookingListItem current = listItems.get(position);
+        if(listItems.get(position).getPhoto() != null) image.setImageBitmap(listItems.get(position).getPhoto());
+        else image.setImageResource(R.drawable.art_default_profil);
+        txtNamaAkun.setText(current.getUseradik());
+        txtTugas.setText(current.getTxtDate());
+        txtTanyaTerima.setText("Terima ?");
+
+        btnTidak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userkakak = current.getUserkakak();
+                useradik = current.getUseradik();
+                id = current.getIdJadwal();
+
+                new tidakTask().execute("");
+                listItems.remove(position);
+                BookingListAdapter adapter = new BookingListAdapter(context, listItems);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        btnYa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userkakak = current.getUserkakak();
+                useradik = current.getUseradik();
+                id = current.getIdJadwal();
+
+                new yaTask().execute("");
+                listItems.remove(position);
+                BookingListAdapter adapter = new BookingListAdapter(context, listItems);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
 
         return convertView;
+    }
+
+    public String tekanIya() {
+
+
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(url+"jawaban=1&userkakak="+userkakak+"&useradik="+useradik+"&id_jadwal="+id);
+            System.out.println("ini user kakak, adik,id"+userkakak+" "+useradik+" "+id);
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            is = entity.getContent();
+            //debug
+            System.out.println(is);
+            Log.e("pass 1", "connection success ");
+        } catch (Exception e) {
+            Log.e("Fail 1", e.toString());
+//            Toast.makeText(, "Invalid IP Address",
+//                    Toast.LENGTH_LONG).show();
+
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader
+                    (new InputStreamReader(is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            result = sb.toString();
+            //debug
+
+            Log.e("pass 2", "connection success ");
+            System.out.println(result);
+        } catch (Exception e) {
+            Log.e("Fail 2", e.toString());
+
+        }
+        return result;
+    }
+
+
+    public String tekanTidak() {
+
+
+
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(url+"jawaban=0&userkakak="+userkakak+"&useradik="+useradik+"&id_jadwal="+id);
+            System.out.println("ini user kakak, adik,id"+userkakak+" "+useradik+" "+id);
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            is = entity.getContent();
+            //debug
+            System.out.println(is);
+            Log.e("pass 1", "connection success ");
+        } catch (Exception e) {
+            Log.e("Fail 1", e.toString());
+//            Toast.makeText(, "Invalid IP Address",
+//                    Toast.LENGTH_LONG).show();
+
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader
+                    (new InputStreamReader(is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            result = sb.toString();
+            //debug
+
+            Log.e("pass 2", "connection success ");
+            System.out.println(result);
+        } catch (Exception e) {
+            Log.e("Fail 2", e.toString());
+
+        }
+        return result;
+    }
+
+    class yaTask extends AsyncTask<String, String, String> {
+
+        protected String doInBackground(String... params) {
+            String hasil = tekanIya();
+            return hasil;
+        }
+        protected void onPostExecute(String result) {
+//            if (result.equals("OK")){
+            Toast.makeText(context, "Berhasil Dikonfirmasi", Toast.LENGTH_LONG).show();
+            BookingListAdapter.this.notifyDataSetChanged();
+//            }
+            //after background is done, use this to show or hide dialogs
+        }
+
+    }
+    class tidakTask extends AsyncTask<String, String, String> {
+
+        protected String doInBackground(String... params) {
+            String hasil = tekanTidak();
+            return hasil;
+        }
+
+
+        protected void onPostExecute(String result) {
+//            if (result.equals("OK")){
+            Toast.makeText(context, "Berhasil Dikonfirmasi", Toast.LENGTH_LONG).show();
+            BookingListAdapter.this.notifyDataSetChanged();
+//            }
+            //after background is done, use this to show or hide dialogs
+        }
     }
 }
